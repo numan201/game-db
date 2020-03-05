94129871e3ff78024a410765469d2429a279524f
possumrapture <36344735+possumrapture@users.noreply.github.com>
2020-03-05 17:42:13 -0600

    getIssuesNumbers() makes less requests & getCommitNumbers() made to work for private repo
    getIssuesNumbers() makes 1 request, instead of 6

--- routes/about.js
+++ routes/about.js
@@ -3,12 +3,56 @@ const router = express.Router();
 const axios = require('axios');
 const { gitHubKey } = require('../keys');
 
-let githubIdList = ["team", "possumrapture", "numan201", "johnnguyen3196", "justAlejandro", "rambisco"];
+let githubIdList = ["team", "possumrapture", "numan201", "johnnguyen3196", "JustAlejandro", "rambisco", "Numan Habib"];
+
+function getIssuesNumbers() {
+    let issueNumbers = Array(6).fill(0);
+    let promises = [];
+
+    let url = "https://api.github.com/search/issues?q=repo:numan201/game-db type:issue";
+
+    let config = {
+        method: 'get',
+        headers: {"Authorization" : 'Token ' + gitHubKey}
+    };
+
+    let request = axios.get(url, config);
+    promises.push(request);
+
+    return axios.all(promises).then( (responses) => {
+        responses.forEach ( (response, i) => {
+            issueNumbers[i] = response.data.items.length;
+
+            response.data.items.forEach((item) => {
+                for(let j = 0; j < item.assignees.length; j++) {
+                    let issueAssignee = item.assignees[j].login;
+                    if(githubIdList.includes(issueAssignee)) {
+                        let assigneeIndex = githubIdList.indexOf(issueAssignee);
+                        issueNumbers[assigneeIndex] ++;
+                    }
+                }
+            });
+        });
+        return issueNumbers;
+    });
+}
 
 
 function getCommitNumbers() {
     let commitNumbers = [];
     let promises = [];
+
+    let today = new Date();
+    let date = today.getFullYear() + "-";
+
+    if((today.getMonth()+1) < 11)
+        date += "0" +(today.getMonth()+1) + "-";
+    else
+        date += (today.getMonth()+1);
+
+    if(today.getDate() < 10)
+        date += "0" + today.getDate();
+    else date += today.getDate();
 
     for (let i = 0; i < githubIdList.length; i++) {
         let url = "https://api.github.com/search/commits?q=";
@@ -16,7 +60,8 @@ function getCommitNumbers() {
         if(i !== 0) {
             url += "author:" + githubIdList[i] + " ";
         }
-        url += "repo:numan201/game-db author-date:2020-01-01..2020-06-01";
+
+        url += "repo:numan201/game-db author-date:2020-01-01.." + date;
 
         let config = {
             method: 'get',
@@ -28,47 +73,19 @@ function getCommitNumbers() {
 
         let request = axios.get(url, config);
         promises.push(request);
-
     }
 
     // Wait for all requests to finish
     return axios.all(promises).then( (responses) => {
-        responses.forEach ( (response, i) => {
-            commitNumbers[i] = response.data.items.length;
+        responses.forEach ((response, i) => {
+            //Need to have this check because Numan committed under 2 names
+            if(i === 6)
+                commitNumbers[commitNumbers.indexOf("numan201")] += response.data.total_count;
+            else
+                commitNumbers[i] = response.data.total_count;
         });
 
         return commitNumbers;
-    });
-
-}
-
-function getIssuesNumbers() {
-    let issueNumbers = [];
-    let promises = [];
-
-    for (let i = 0; i < githubIdList.length; i++) {
-        let url = "https://api.github.com/search/issues?q=";
-
-        if(i !== 0) {
-            url += "author:" + githubIdList[i] + " ";
-        }
-        url += "repo:numan201/game-db type:issue";
-
-        let config = {
-            method: 'get',
-            headers: {"Authorization" : 'Token ' + gitHubKey}
-        };
-
-        let request = axios.get(url, config);
-        promises.push(request);
-
-    }
-
-    return axios.all(promises).then( (responses) => {
-        responses.forEach ( (response, i) => {
-            issueNumbers[i] = response.data.items.length;
-        });
-        return issueNumbers;
     });
 
 }
