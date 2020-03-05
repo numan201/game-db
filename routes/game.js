1a9e21d40cbf3ca2fddd32010b7a62bb4ac1643c
rambisco <david.wolf.tx@gmail.com>
2020-03-04 19:54:59 -0600

    embedded twitch player on game pages. game ejs file changed to add twitch player, game js now has two API requests to recieve username to pass to ejs

--- routes/game.js
+++ routes/game.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
 var youtubeApiV3Search = require("youtube-api-v3-search");
+const axios = require('axios');
+
 
 /* GET games listing. */
 router.get('/', function(req, res, next) {
     let id = require('mongodb').ObjectID(req.query.id);
+    let username;
 
     req.app.locals.db.collection('games').findOne({_id : id}, (err, game) => {
         req.app.locals.db.collection('developers').
@@ -13,7 +15,52 @@ router.get('/', function(req, res, next) {
                 req.app.locals.db.collection('publishers').
                     aggregate([{$unwind: "$games"}, {$match: { "games.id" : game.id}}, {$project: {"name": 1}}]).toArray((err, publishers) => {
 
-                        // Reviews counts
+                    axios({ //request number 1. get game id.
+                        method: 'get',
+                        url: "https://api.twitch.tv/helix/games",
+                        params: {
+                            name: game.name
+                        },
+                        headers: {
+                            'Client-ID': 'yqjk0ha999ss01nnisfcac3734ig7t'
+                        }
+                    })
+                        .then(function (response) {
+                           let game_id = response.data.data[0].id;
+                            console.log("RESPOOOOOOONSE: " + JSON.stringify(game_id)); // JSON.stringify(response.data)
+
+
+                            axios({ //request 2. get top streamer from game id.
+                                method: 'get',
+                                url: "https://api.twitch.tv/helix/streams",
+                                params: {
+                                    game_id: game_id,
+                                    first: 1 //only top stream
+                                },
+                                headers: {
+                                    'Client-ID': 'yqjk0ha999ss01nnisfcac3734ig7t'
+                                }
+                            })
+                                .then(function (response) {
+                                    let resp = response.data.data;
+                                    username = resp[0]["user_name"];
+                                    console.log("RESPOOOOOOONSE: " + JSON.stringify(username)); // JSON.stringify(response.data)
+
+                                })
+                                .catch(function (error) {
+                                    console.log(error);
+                                })
+                                .then(function (response) {
+                                    // always executed
+                                });
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        })
+                        .then(function (response) {
+                            // always executed
+                        });
+                    // Reviews counts
                         let reviewsCounts = [0, 0, 0, 0, 0];
                         game.ratings.forEach((rating) => {
                             reviewsCounts[rating.id - 1] = rating.count;
@@ -27,11 +74,11 @@ router.get('/', function(req, res, next) {
                         };
                         let result = youtubeApiV3Search("AIzaSyAkzg-fJPr1Gt1Qu3o7ba_2FvpvloXQyzo", options, (err, response) => {
                             let videos = [];
-                            console.log(response);
+                            //console.log(response);
                             response.items.forEach((item) =>{
                                 videos.push(item.id.videoId);
                             })
-                            res.render('game', {title: game.name, game: game, developers: developers, publishers: publishers, reviewsCounts: JSON.stringify(reviewsCounts), videos: videos});
+                            res.render('game', {username: username, title: game.name, game: game, developers: developers, publishers: publishers, reviewsCounts: JSON.stringify(reviewsCounts), videos: videos});
                         });
 
                 });
