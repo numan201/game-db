70d4192c79a19690503982481484cd1c2fe6b580
Numan Habib <numanhabib@gmail.com>
2020-03-24 22:21:45 -0500

    Handling of no Twitch stream\Steam News + Developer\Publisher Page Refactoring

--- routes/publisher.js
+++ routes/publisher.js
@@ -20,45 +20,43 @@ class UniqueSet {
 /* GET games listing. */
 router.get('/', function(req, res, next) {
     let id = require('mongodb').ObjectID(req.query.id);
-
     let developerList = new UniqueSet();
-    req.app.locals.db.collection('publishers').findOne({_id : id}, (err, publisher) => {
-        //Making a promise to for synchronous
-        let waitGame = new Promise((resolve, reject) => {
-            publisher.games.forEach((game, i) => {
-                req.app.locals.db.collection('games').findOne({id: game.id}, {name: 1, background_image: 1, released: 1, reviews_count: 1}, (err, gameFound) => {
-                    if (gameFound == null) {
-                        console.log("ENTRY FOR GAME " + publisher.games[i].name + " NOT FOUND");
-                        publisher.games.splice(i, 1);
-                        if (i == publisher.games.length) resolve();
-                    }
-                    else {
-                        game.instance = gameFound;
 
-                        let promise = new Promise(resolve1 => {
-                            req.app.locals.db.collection('developers').aggregate([{ $unwind: "$games" }, { $match: { "games.id": game.instance.id } },
-                            { $project: { "name": 1, games_count: 1, image_background: 1 } }]).toArray().then((developers) => {
+    // Get publisher
+    req.app.locals.db.collection('publishers').findOne({_id : id}).then((publisher) => {
 
-                                developers.forEach((developer, j) => {
-                                    developerList.add(developer);
-                                    if (j == developers.length - 1) resolve1();
-                                });
-                                if (developers.length == 0) resolve1();
-                            });
-                        });
+        let gameIds = [];
+        publisher.games.forEach((game) => {
+            gameIds.push(game.id);
+        });
 
-                        promise.then(() => {
-                            if (i == publisher.games.length - 1) resolve();
-                        });
-                    }
-                });
+        // Get games objects for publisher
+        req.app.locals.db.collection('games').find({id: {$in: gameIds}}, {name: 1, background_image: 1, released: 1, reviews_count: 1}).toArray().then( (games) => {
+            let gamesDevelopersPromises = [];
+
+            // Get developers for each game
+            games.forEach((game) => {
+                let gamesDevelopersPromise = req.app.locals.db.collection('developers').aggregate([{$unwind: "$games"}, {$match: {"games.id": game.id}}, {$project: {name: 1, games_count: 1, image_background: 1}}]).toArray();
+                gamesDevelopersPromises.push(gamesDevelopersPromise);
             });
+
+            return Promise.all(gamesDevelopersPromises).then((gamesDevelopersPromises) => {
+                gamesDevelopersPromises.forEach((gamesDevelopersPromises) => {
+                    gamesDevelopersPromises.forEach((developer) => {
+                        developerList.add(developer);
+                    })
+                });
+
+                return games;
+            });
+
+        }).then((games) => {
+            let developers = developerList.values();
+            res.render('publisher', {title: publisher.name, publisher: publisher, games: games, developers: developers});
         });
 
-        waitGame.then(() => {
-            publisher.developers = developerList.values();
-            res.render('publisher', {title: publisher.name, publisher: publisher, games: publisher.games, developers: publisher.developers});
-        });
+
+
     });
 
 });
