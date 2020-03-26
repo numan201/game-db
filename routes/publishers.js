e2ec6d643e53aeea447782d18a86adc07f319775
Numan Habib <numanhabib@gmail.com>
2020-03-25 20:46:34 -0500

    - Sort biggest developers\publishers first (descending games count) - Publishers pagination bug fix

--- routes/publishers.js
+++ routes/publishers.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 
     let currentPage = getCurrentPage(req);
 
-    req.app.locals.db.collection('publishers').find().limit(20).toArray().then((publishers) => {
+    req.app.locals.db.collection('publishers').find().sort({games_count: -1}).skip(skipCalc(currentPage)).limit(resultsPerPage).toArray().then((publishers) => {
         req.app.locals.db.collection('publishers').countDocuments().then((count) => {
             res.render('publishers', {title: 'Publishers', pagination: paginationObject(currentPage, count), publishers: publishers});
         });
