892195782839af6db3febba29c6578f234e2c211
john <johnnguyen3196@gmail.com>
2020-04-13 16:48:17 -0500

    added filtering for developers and publishers with radio buttons

--- routes/publishers.js
+++ routes/publishers.js
@@ -10,30 +10,14 @@ router.get('/', function(req, res, next) {
     let filtersCondition = {$match : {}};
     let paginationQuery = [{$skip: skipCalc(currentPage)}, {$limit: resultsPerPage} ];
 
-    if('numbers' in req.query){
+    if('numbers' in req.query) {
         let numbers = req.query.numbers;
-        if (!Array.isArray(req.query.numbers)) {
-            numbers = parseInt(numbers);
-            if(numbers === 500){
-                filtersCondition['$match']['games_count'] = {$gt: numbers};
-            } else {
-                filtersCondition['$match']['games_count'] = {$gt: numbers, $lt: numbers + 50};
-            }
+
+        numbers = parseInt(numbers);
+        if (numbers === 500) {
+            filtersCondition['$match']['games_count'] = {$gt: numbers};
         } else {
-            let parsedNumbers = [];
-            numbers.forEach((number) => {
-                parsedNumbers.push(parseInt(number));
-            });
-            numbers = parsedNumbers;
-            let filterArray = [];
-            numbers.forEach((number) => {
-                if(number === 500) {
-                    filterArray.push({$gt: 500});
-                } else{
-                    filterArray.push({$gt: number, $lt: number + 50});
-                }
-            });
-            filtersCondition['$match']['games_count'] = filterArray;
+            filtersCondition['$match']['games_count'] = {$gt: numbers, $lt: numbers + 50};
         }
     }
 
@@ -44,6 +28,10 @@ router.get('/', function(req, res, next) {
     let publishersCountPromise = req.app.locals.db.collection('publishers').aggregate(countQuery).toArray();
 
     Promise.all([publishersPromise, publishersCountPromise]).then(([publishers, count]) => {
+        if(count.length === 0){
+            let totalCount = 0;
+            count[0] = {totalcount: totalCount};
+        }
         res.render('publishers', {
             title: 'Publishers',
             pagination: paginationObject(currentPage,  count[0].totalCount, req.query),
