7511b7a5a2a549f533f977a833feef79fe878d79
john <johnnguyen3196@gmail.com>
2020-04-13 15:11:12 -0500

    filtering publishers somewhat works

--- routes/publishers.js
+++ routes/publishers.js
@@ -7,9 +7,47 @@ router.get('/', function(req, res, next) {
 
     let currentPage = getCurrentPage(req);
 
-    req.app.locals.db.collection('publishers').find().sort({games_count: -1}).skip(skipCalc(currentPage)).limit(resultsPerPage).toArray().then((publishers) => {
-        req.app.locals.db.collection('publishers').countDocuments().then((count) => {
-            res.render('publishers', {title: 'Publishers', pagination: paginationObject(currentPage, count, req.query), publishers: publishers});
+    let filtersCondition = {$match : {}};
+    let paginationQuery = [{$skip: skipCalc(currentPage)}, {$limit: resultsPerPage} ];
+
+    if('numbers' in req.query){
+        let numbers = req.query.numbers;
+        if (!Array.isArray(req.query.numbers)) {
+            numbers = parseInt(numbers);
+            if(numbers === 500){
+                filtersCondition['$match']['games_count'] = {$gt: numbers};
+            } else {
+                filtersCondition['$match']['games_count'] = {$gt: numbers, $lt: numbers + 50};
+            }
+        } else {
+            let parsedNumbers = [];
+            numbers.forEach((number) => {
+                parsedNumbers.push(parseInt(number));
+            });
+            numbers = parsedNumbers;
+            let filterArray = [];
+            numbers.forEach((number) => {
+                if(number === 500) {
+                    filterArray.push({$gt: 500});
+                } else{
+                    filterArray.push({$gt: number, $lt: number + 50});
+                }
+            });
+            filtersCondition['$match']['games_count'] = filterArray;
+        }
+    }
+
+    let paginatedQuery = [filtersCondition].concat(paginationQuery);
+    let countQuery = [filtersCondition].concat( { $count: 'totalCount' });
+
+    let publishersPromise = req.app.locals.db.collection('publishers').aggregate(paginatedQuery).toArray();
+    let publishersCountPromise = req.app.locals.db.collection('publishers').aggregate(countQuery).toArray();
+
+    Promise.all([publishersPromise, publishersCountPromise]).then(([publishers, count]) => {
+        res.render('publishers', {
+            title: 'Publishers',
+            pagination: paginationObject(currentPage,  count[0].totalCount, req.query),
+            publishers: publishers
         });
     });
 });
