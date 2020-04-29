140d26e8d80934694926fa1dacd5309b250a6211
Numan Habib <numanhabib@gmail.com>
2020-04-29 00:00:00 -0500

    About route removed unncessary "async" keyword

--- routes/about.js
+++ routes/about.js
@@ -4,14 +4,12 @@ const axios = require('axios');
 const {getData} = require("../models/about");
 
 /* GET about */
-router.get('/', async function(req, res) {
+router.get('/', function(req, res) {
 
     getData(req.app.locals.db).then((aboutData) => {
         aboutData = Object.assign(aboutData, {title: 'About', page: req.baseUrl});
         res.render('about', aboutData);
     });
-
-
 
 });
 
